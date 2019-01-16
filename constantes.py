c524c4d67eb33c77fc91d4548d1907864c00fc82
hakander <hakanOC78@hotmail.com>
2019-01-16 16:31:29 +0100

    constantes

---
+++ constantes.py
@@ -0,0 +1,9 @@
+number_sprite = 15
+size_sprite = 41
+corner_window = number_sprite * size_sprite
+
+image_start = "ressource/start.png"
+image_arrive = "ressource/arrive.png"
+image_wall = "ressource/wall.png"
+image_home = "ressource/floor-tiles-20x20.png"
+image_empty = "ressource/empty.png"
